108ad9a4261c8ed993a34834c5508c3297051d66
thenaivekid <neupane.ashok.9696@gmail.com>
2026-02-02 15:43:05 +0100

    correct num particles 4

--- core/fk_sampling.py
+++ core/fk_sampling.py
@@ -184,7 +184,7 @@ def run_fk_sampling(config, stage_idx=None, logger=None, wandb_run=None, pipelin
     # SAMPLING LOOP
     pipeline.unet.eval()
     
-    num_particles = 2 # 4
+    num_particles = 4 # 4
     samples = []
     split_steps = [config.split_step]
     split_times = [config.split_time]
