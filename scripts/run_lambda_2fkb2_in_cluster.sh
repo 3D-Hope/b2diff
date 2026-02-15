c39e356a249e4d066fa10f4b7578bfb2a07ad1f2
thenaivekid <neupane.ashok.9696@gmail.com>
2026-02-15 12:33:31 +0100

    run in msp

--- scripts/run_lambda_2fkb2_in_cluster.sh
+++ scripts/run_lambda_2fkb2_in_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=lambda_2_fk_4particles
 #SBATCH --partition=batch
-#SBATCH --constraint=zone-sof1
+#SBATCH --constraint=zone-msp3
 #SBATCH --gpus=h200:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=12G
