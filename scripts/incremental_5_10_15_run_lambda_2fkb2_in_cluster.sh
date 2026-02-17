03b1d5ffa6de5d83a2f2679de76399a450edf17a
Saugat2002 <saugatadhikari.2002@gmail.com>
2026-02-17 09:06:10 +0100

    added incremental 5 10 15

--- scripts/incremental_5_10_15_run_lambda_2fkb2_in_cluster.sh
+++ scripts/incremental_5_10_15_run_lambda_2fkb2_in_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=10_only_lambda_2_fk_4particles
+#SBATCH --job-name=incremental_5_10_15_only_lambda_2_fk_4particles
 #SBATCH --partition=batch
 #SBATCH --constraint=zone-msp3
 #SBATCH --gpus=h200:1
@@ -160,7 +160,7 @@ echo "GPUs detected: ${NUM_GPUS}"
     # pipeline.continue_from_stage=110 \
     # resume_id="tg2dp40a" \
 
-run_name="10_only_lambda_2_fk_4particles"
+run_name="incremental_5_10_15_only_lambda_2_fk_4particles"
 # sample.batch_size=2, means 2 prompts are sampled, each has 4 particles for best and 4 for worse reward if boest_only_fk is false else only 4 particles for best reward only no worst
 # batch size for sampling 12 for only best and 6 for both best and worst
 python3 ./scripts/training/train_pipeline.py \
