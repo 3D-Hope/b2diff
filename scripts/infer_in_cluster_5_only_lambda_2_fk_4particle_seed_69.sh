0fbf937d8cc61d71a43789c59edf5f3f393c0eff
ajad chhatkuli <ajad.chhatkuli@gmail.com>
2026-02-20 12:24:02 +0100

    seed 60

--- scripts/infer_in_cluster_5_only_lambda_2_fk_4particle_seed_69.sh
+++ scripts/infer_in_cluster_5_only_lambda_2_fk_4particle_seed_69.sh
@@ -132,7 +132,7 @@ echo "Training started at: ${START_TIME_READABLE}"
 echo "GPUs detected: ${NUM_GPUS}"
 
 run_name="5_only_lambda_2_fk_4particle_seed_69"
-stage_number=33
+stage_number=40
 python3 ./scripts/inference/inference_lora_clip_reward.py \
 --checkpoint_path /home/pramish_paudel/codes/b2diff/model/lora/${run_name}/stage${stage_number}/checkpoints/checkpoint_1/ \
 --output_dir ./outputs/${run_name}/stage${stage_number} \
