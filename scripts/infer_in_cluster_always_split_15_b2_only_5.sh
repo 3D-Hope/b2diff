12e6198ae4ac2c2aef399f421e1fe364b23b987e
Saugat2002 <saugatadhikari.2002@gmail.com>
2026-02-20 09:15:27 +0100

    changed stages for 3 inf

--- scripts/infer_in_cluster_always_split_15_b2_only_5.sh
+++ scripts/infer_in_cluster_always_split_15_b2_only_5.sh
@@ -132,7 +132,7 @@ echo "Training started at: ${START_TIME_READABLE}"
 echo "GPUs detected: ${NUM_GPUS}"
 
 run_name="always_split_15_b2_only_5"
-stage_number=37
+stage_number=29
 python3 ./scripts/inference/inference_lora_clip_reward.py \
 --checkpoint_path /home/pramish_paudel/codes/b2diff/model/lora/${run_name}/stage${stage_number}/checkpoints/checkpoint_1/ \
 --output_dir ./outputs/${run_name}/stage${stage_number} \
