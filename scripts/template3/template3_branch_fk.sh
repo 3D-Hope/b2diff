9192f8dcadd656a875a84232fbe272bf42976631
Saugat2002 <saugatadhikari.2002@gmail.com>
2026-02-27 06:39:02 +0100

    template3

--- scripts/template3/template3_branch_fk.sh
+++ scripts/template3/template3_branch_fk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=template3_branch_fk
+#SBATCH --job-name=template3_branch_fk_new
 #SBATCH --partition=batch
-#SBATCH --constraint=zone-msp3
+#SBATCH --constraint=zone-sof1
 #SBATCH --gpus=h200:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=12G
@@ -161,7 +161,7 @@ echo "GPUs detected: ${NUM_GPUS}"
     # pipeline.continue_from_stage=110 \
     # resume_id="tg2dp40a" \
 
-run_name="template3_branch_fk"
+run_name="template3_branch_fk_new"
 # sample.batch_size=2, means 2 prompts are sampled, each has 4 particles for best and 4 for worse reward if boest_only_fk is false else only 4 particles for best reward only no worst
 # batch size for sampling 12 for only best and 6 for both best and worst
 python3 ./scripts/training/train_pipeline.py \
@@ -190,9 +190,9 @@ python3 ./scripts/training/train_pipeline.py \
     train.max_grad_norm=0.005 \
     train.incremental_timesteps=[4,8,12,16] \
     train.num_stages_per_increment=10 \
-    prompt_file=configs/prompt/template3_train.json \
-    resume_id="v310a3hb" \
-    pipeline.continue_from_stage=92
+    prompt_file=configs/prompt/template3_train.json
+    # resume_id="v310a3hb" \
+    # pipeline.continue_from_stage=92
 
 
 
